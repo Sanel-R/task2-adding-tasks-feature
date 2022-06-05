bff791fc1e93da973dbcc302a6db6677af90a955
Sanele <Sanele@Sanele>
2022-06-05 02:24:29 +0200

    Console based application

---
+++ Task 2 - EasyKanban/src/Main.java
@@ -0,0 +1,155 @@
+import java.util.Scanner;
+
+public class Main 
+{
+	
+	public static void main(String[] args) 
+	{
+		// welcome user(s)
+		System.out.println("Welcome to EasyKanban\n");
+		
+		// Used to prompt the user(s)
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.println("You can choose the following list of options: \n1.)Add tasks\n2.)Show report\n3.)Quit\n\n");
+		
+		// prompts the user to enter option.
+		System.out.print("Enter option: ");int option = stdIn.nextInt();
+		
+		switch (option) {
+		case 1: 
+		{
+			//implement a method to handle this cleanup.
+			addingTask();
+			break;
+		}
+		case 2:
+		{
+			System.out.println("Coming Soon");
+			break;
+		}
+		case 3:
+		{
+			System.out.println("Quit");
+			break;
+		}
+		default:
+			stdIn.close();
+			throw new IllegalArgumentException("Unexpected value: " + option + "\nPlease enter valid option.");
+		}
+		
+		//close our scanner method.
+		stdIn.close();
+	}
+	
+	//This method will user the task class to wrapper our code to make it much cleaner.
+	public static Task addingTask()
+	{
+		// Used to prompt the user(s)
+		Scanner stdIn = new Scanner(System.in);
+		
+		// ask the user in advance the number of tasks he/she want to add.
+		System.out.print("Enter the number of tasks you want: "); int numOfTasks = stdIn.nextInt();
+		
+		//add new space.
+		System.out.println();
+		
+		//consume the next line.
+		stdIn.nextLine();
+		
+		// create the 1st instance task that will store the list of tasks.
+		System.out.print("\nEnter your task name: "); String taskName = stdIn.nextLine();
+		System.out.print("\nEnter your task description: "); String taskDescription= stdIn.nextLine();
+		System.out.print("\nEnter your developer detail: "); String developerDetails= stdIn.nextLine();
+		System.out.print("\nEnter your task duration: "); int taskDuration= stdIn.nextInt();
+		stdIn.nextLine();
+		String taskStatus;
+		
+		// options for our task details.
+		System.out.println("\nYou can choose from the following list for task status: \n1.)To Do\n2.)Done\n3.)Doing");
+		
+		System.out.print("\nEnter your task status: "); int option= stdIn.nextInt();
+		stdIn.nextLine();
+		//add neew line
+		System.out.println("\n");
+		switch (option) {
+		case 1: 
+		{
+			
+			taskStatus = "To Do";
+			break;
+		}
+		case 2:
+		{
+			taskStatus = "Done";
+			break;
+		}
+		case 3:
+		{
+			taskStatus = "Doing";
+			break;
+		}
+		default:
+			stdIn.close();
+			throw new IllegalArgumentException("Unexpected value: " + option + "Need to choose from the provide cases.");
+		}
+		
+		
+		// create the first tasks instance.
+		Task listOfTask = new Task(taskName, taskDescription, developerDetails, taskDuration, taskStatus);
+		
+		// decrement the first task entered.
+		numOfTasks -= 1;
+		
+		do
+		{
+			// prompt the user to enter new task until he/she is done.
+			// create the 1st instance task that will store the list of tasks.
+			System.out.print("\nEnter your task name: "); taskName = stdIn.nextLine();
+			System.out.print("\nEnter your task description: "); taskDescription= stdIn.nextLine();
+			System.out.print("\nEnter your developer detail: "); developerDetails= stdIn.nextLine();
+			System.out.print("\nEnter your task duration: "); taskDuration= stdIn.nextInt();
+			stdIn.nextLine();
+			
+			// options for our task details.
+			System.out.println("\nYou can choose from the following list for task status: \n1.)To Do\n2.)Done\n3.)Doing");
+			
+			System.out.print("\nEnter your task status: "); option= stdIn.nextInt();
+			//add new line
+			stdIn.nextLine();
+			System.out.println("\n");
+			switch (option) {
+			case 1: 
+			{
+				
+				taskStatus = "To Do";
+				break;
+			}
+			case 2:
+			{
+				taskStatus = "Done";
+				break;
+			}
+			case 3:
+			{
+				taskStatus = "Doing";
+				break;
+			}
+			default:
+				stdIn.close();
+				throw new IllegalArgumentException("Unexpected value: " + option + "Need to choose from the provide cases.");
+			}
+			
+			// add this task to our list.
+			listOfTask.addTask(new Task(taskName, taskDescription, developerDetails, taskDuration, taskStatus));
+			
+			// decrement the number of tasks.
+			numOfTasks-=1;
+		}while (numOfTasks != 0);
+		
+		stdIn.close();
+		
+		// return all the task object.
+		return listOfTask;
+	}
+}
---
+++ Task 2 - EasyKanban/src/Task.java
@@ -0,0 +1,98 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author Hopewell
+ * Handles all the logic of adding a new task.
+ */
+public class Task 
+{
+	// Define all our variables as private to handle that logic.
+	private String taskName;
+	private int taskNumber;
+	private String taskDescription;
+	private String developerDetails;
+	private int taskDuration;
+	private String taskID;
+	private String taskStatus;
+	
+	// create a container that will store all our Task instances.
+	private ArrayList<Task> taskList = new ArrayList<>();
+	
+	// keep track of the task numbers.
+	private int lastTaskNumbers = 0;
+	
+	// define our constructor.
+	public Task(String taskName,String taskDescription, String developerDetails, int taskDuration, String taskStatus)
+	{
+		// initialize all our local variables of the class.
+		this.taskName = taskName;
+		this.taskNumber = 0;
+		this.taskDescription = taskDescription;
+		
+		//check if the description is < 50 if not prompt the user to enter something less then 50 before processing.
+		if (!this.checkTaskDescription())
+		{
+			// prompt the user to re-enter the description
+			Scanner stIn = new Scanner(System.in);
+			do
+			{
+				System.out.print("Please enter a task description of less than 50 characters: "); this.taskDescription = stIn.nextLine();
+			}
+			while(!this.checkTaskDescription());
+			stIn.close();
+		}
+		
+		this.developerDetails = developerDetails;
+		this.taskDescription = taskDescription;
+		this.taskStatus = taskStatus;
+		this.taskID = this.createTaskID();
+		this.taskDuration = taskDuration;
+	}
+	
+	// define our methods.
+	public void addTask(Task task)
+	{
+		// increment.
+		lastTaskNumbers += 1;
+		task.taskNumber = lastTaskNumbers;
+		taskList.add(task);
+	}
+	
+	public Boolean checkTaskDescription()
+	{
+		// check if length is less then 50.
+		return this.taskDescription.length() < 50;
+	}
+	
+	public String createTaskID()
+	{
+		//NB: This method will work correcetly if our user enter name and surname 1. for each e.g. Zanele Ntwasa {Ans the two strings are delimeted by a space}
+		String[] splitNameAndSurname = this.developerDetails.split("\\s+");
+		String abbrTaskName = this.taskDescription.substring(0, 2).toUpperCase();
+		String abbrName = splitNameAndSurname[0].substring(splitNameAndSurname[0].length() - 3, splitNameAndSurname[0].length()).toUpperCase();
+		
+		return String.format("%s:%s:%s", abbrTaskName, this.taskNumber, abbrName);
+	}
+	
+	public String printTaskDetails()
+	{
+		return String.format("Task Status: %s \nDeveloper details: %s \nTask number: %s \nTask description: %s \nTask ID: %s \nTask duration: %s \n\n", this.taskStatus, this.developerDetails, this.taskNumber, this.taskDescription, this.taskID, this.taskDuration);
+	}
+	
+	public int returnTotalHours()
+	{
+		int sumTotalCount = 0;
+		
+		for (int i = 0; i < taskList.size(); i++)
+		{
+			sumTotalCount += taskList.get(i).taskDuration;
+		}
+		
+		return sumTotalCount + this.taskDuration;
+	}
+}
